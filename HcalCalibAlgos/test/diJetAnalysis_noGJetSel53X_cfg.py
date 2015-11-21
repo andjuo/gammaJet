0c8385fc9470489e05b08303114f0755191839a7
Andrius Juodagalvis <andjuo@hotmail.com>
2015-11-21 11:13:19 +0100

    diJetAnalysis_noGJetSel53X_cfg.py: remove photonID and adapt for 53X MC

--- HcalCalibAlgos/test/diJetAnalysis_noGJetSel53X_cfg.py
+++ HcalCalibAlgos/test/diJetAnalysis_noGJetSel53X_cfg.py
@@ -21,24 +21,24 @@ process.GlobalTag.globaltag = 'PHYS14_25_V1::All'
 
 # get the official photonID producer
 #process.load("RecoEgamma/PhotonIdentification/PhotonIDValueMapProducer_cfi")
-process.load("RecoEgamma/PhotonIdentification/egmPhotonIDs_cff")
+#process.load("RecoEgamma/PhotonIdentification/egmPhotonIDs_cff")
 
-from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+#from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
 # turn on VID producer, indicate data format  to be
 # DataFormat.AOD or DataFormat.MiniAOD, as appropriate
 #if useAOD == True :
-dataFormat = DataFormat.AOD
+#dataFormat = DataFormat.AOD
 #else :
 #dataFormat = DataFormat.MiniAOD
 
-switchOnVIDPhotonIdProducer(process, dataFormat)
+#switchOnVIDPhotonIdProducer(process, dataFormat)
 
 # define which IDs we want to produce
-my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_PHYS14_PU20bx25_V2_cff']
+#my_id_modules = ['RecoEgamma.PhotonIdentification.Identification.cutBasedPhotonID_PHYS14_PU20bx25_V2_cff']
 
 #add them to the VID producer
-for idmod in my_id_modules:
-          setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
+#for idmod in my_id_modules:
+#          setupAllVIDIdsInModule(process,idmod,setupVIDPhotonSelection)
 
 
 #load the response corrections calculator
@@ -79,14 +79,22 @@ process.GammaJetAnalysis.jet2EtMax = cms.double(1e16)
 process.GammaJetAnalysis.jet3EtMax = cms.double(30.) # keep
 
 
+# for 5_3_X
+process.GammaJetAnalysis.photonCollName=cms.string('photons')
+process.GammaJetAnalysis.electronCollName= cms.string("gsfElectrons")
+process.GammaJetAnalysis.photonIdLooseName= cms.InputTag("PhotonIDProd","PhotonCutBasedIDLoose")
+process.GammaJetAnalysis.photonIdTightName= cms.InputTag("PhotonIDProd","PhotonCutBasedIDTight")
+process.GammaJetAnalysis.pfJetCollName= cms.string('ak5PFJetsCHS')
+process.GammaJetAnalysis.pfJetCorrName= cms.string('ak5PFchsL2L3')
+process.GammaJetAnalysis.genJetCollName= cms.string('ak5GenJets')
 
 
 # a clone without CHS
-process.GammaJetAnalysis_noCHS= process.GammaJetAnalysis.clone()
-process.GammaJetAnalysis_noCHS.rootHistFilename = cms.string('diJet_tree_nonCHS_noGJetSel.root')
+#process.GammaJetAnalysis_noCHS= process.GammaJetAnalysis.clone()
+#process.GammaJetAnalysis_noCHS.rootHistFilename = cms.string('diJet_tree_nonCHS_noGJetSel.root')
 # for 7XY use ak4* instead of ak5
-process.GammaJetAnalysis_noCHS.pfJetCollName = cms.string('ak4PFJets')
-process.GammaJetAnalysis_noCHS.pfJetCorrName = cms.string('ak4PFL2L3')
+#process.GammaJetAnalysis_noCHS.pfJetCollName = cms.string('ak4PFJets')
+#process.GammaJetAnalysis_noCHS.pfJetCorrName = cms.string('ak4PFL2L3')
 
 
 import FWCore.Utilities.FileUtils as FileUtils
@@ -94,27 +102,56 @@ listFileName='relval_qcd.txt'
 mylist = FileUtils.loadListFromFile(listFileName)
 
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring( *mylist )
+    #fileNames = cms.untracked.vstring( *mylist )
+                            fileNames=cms.untracked.vstring('file:/tmp/andriusj/file.root')
 )
 
 
 #To have the same number of histograms, do not run over GenJets
 #process.GammaJetAnalysis.doGenJets = cms.bool(False)
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(200) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(20) )
 process.MessageLogger.cerr.FwkReport.reportEvery=cms.untracked.int32(1000)
-process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(False) )
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 # specify 'workOnAOD=1' if the input file is AOD, otherwise put 0
 process.GammaJetAnalysis.workOnAOD = cms.int32(0)
 process.GammaJetAnalysis.doGenJets = cms.bool(True)
 process.GammaJetAnalysis.debug     = cms.untracked.int32(0)
 
-process.egmPhotonIDSequence.remove('photonMVAValueMapProducer')
+#process.egmPhotonIDSequence.remove('photonMVAValueMapProducer')
+
+
+process.ak5PFchsL2Relative = cms.ESProducer(
+    'LXXXCorrectionESProducer',
+    level     = cms.string('L2Relative'),
+    algorithm = cms.string('AK5PFchs')
+)
+process.ak5PFchsL3Absolute = cms.ESProducer(
+     'LXXXCorrectionESProducer',
+     level     = cms.string('L3Absolute'),
+     algorithm = cms.string('AK5PFchs')
+)
+
+process.ak5PFchsL2L3 = cms.ESProducer(
+    'JetCorrectionESChain',
+    correctors = cms.vstring('ak5PFchsL2Relative', 'ak5PFchsL3Absolute')
+    )
+
+
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+process.load('CommonTools.ParticleFlow.PF2PAT_cff')
+process.load("CommonTools.ParticleFlow.pfNoPileUp_cff")
+
+process.seq_ak5PFCHS= cms.Sequence( process.particleFlowPtrs *
+                                    process.pfNoPileUpJMESequence *
+                                    process.ak5PFJetsCHS
+                                    )
 
 process.p = cms.Path(
-    process.photonIDValueMapProducer *
-    process.egmPhotonIDs *
+#    process.photonIDValueMapProducer *
+#    process.egmPhotonIDs *
+    process.seq_ak5PFCHS *
     process.GammaJetAnalysis
 )
 
